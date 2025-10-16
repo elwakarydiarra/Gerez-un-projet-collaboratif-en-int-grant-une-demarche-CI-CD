dbc45f10d964457b5318e82314b2c0c5ae5eabbf
elwakarydiarra <wakary.diarra@gmail.com>
2025-10-16 21:05:37 +0200

    github action test

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://bobapp-back:8080/api'
 };
 
 /*
